373fe780c70e9fbbcdcb11c8346f63dbf4919b8c
Yulia Tryhuba <tryhubayulia@gmail.com>
2023-06-24 01:22:56 +0300

    add API weather

--- scripts/main_page_script.js
+++ scripts/main_page_script.js
@@ -1,16 +1,16 @@
 // SLIDER
-document.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper('.offers__slider', {
-      slidesPerView: 'auto',
-      spaceBetween: 1,
-      loop: true,
-      autoplay: {
-        delay: 1500,
-        disableOnInteraction: false,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var swiper = new Swiper('.offers__slider', {
+//       slidesPerView: 'auto',
+//       spaceBetween: 1,
+//       loop: true,
+//       autoplay: {
+//         delay: 1500,
+//         disableOnInteraction: false,
+//       },
+//       navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     });
+//   });
