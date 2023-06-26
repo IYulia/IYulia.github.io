d62f4311026a8e2394c9bfcc473b46d33836f91c
Yulia Tryhuba <tryhubayulia@gmail.com>
2023-06-27 02:31:13 +0300

    Add work with API

--- scripts/weather_page_script.js
+++ scripts/weather_page_script.js
@@ -19,19 +19,84 @@ async function searchWeather() {
         }
 
         const { lat, lon } = geoData[0];
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ad0e5d911d6ca005bd9012bcc9c8a180&lang=ua`;
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=ad0e5d911d6ca005bd9012bcc9c8a180&lang=ua`;
 
         const weatherResponse = await fetch(weatherUrl);
         const weatherData = await weatherResponse.json();
-        console.log(weatherData);
-        writeWeather(weatherData);
+
+        createWeather(country, weatherData);
 
     } catch (error) {
         console.log('An error occurred while fetching weather data:', error);
     }
 }
 
-function writeWeather(weatherData) {
-    let weather = weatherData.weather[0];
-    console.log(weather);
+function createWeather(country, weatherData) {
+    const weather = weatherData.weather[0];
+    const divWeatherInfo = document.getElementById('weather-info');
+    let weatherDescription = weather.description;
+    let temp = Math.floor(weatherData.main.temp);
+    let sunrise = weatherData.sys.sunrise;
+    let sunset = weatherData.sys.sunset;
+    let wind = weatherData.wind.speed;
+
+    let sunriseTime = getSunriseSunsetTime(sunrise);
+    let sunsetTime = getSunriseSunsetTime(sunset);
+
+    const html = `<h1 class="weatherTitel">${country}</h1>
+                  <img class="weatherImg" src="https://openweathermap.org/img/wn/${weather.icon}.png" alt="">
+                  <p class="weatherDescription">${weatherDescription}</p>
+                  <h2 class="weatherTemp">${temp}&#8451;</h2>
+                  <p class="sunriseTime">Sunrise: ${sunriseTime}</p>
+                  <p class="sunsetTime">Sunset: ${sunsetTime}</p>
+                  <p class="windSpeed">Wind Speed: ${wind} m/s</p>`;
+
+    divWeatherInfo.innerHTML = html;
 }
+
+function getSunriseSunsetTime(timestamp) {
+    const totalSeconds = timestamp;
+    const date = new Date(totalSeconds * 1000);
+    const timeString = date.toLocaleTimeString().substr(0, 8);
+    return timeString;
+}
+
+
+// async function searchWeather() {
+//     const countryInput = document.getElementById('country');
+//     const country = countryInput.value;
+
+//     if (!country) {
+//         console.log('Please enter a country.');
+//         return;
+//     }
+
+//     const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${country}&limit=1&appid=ad0e5d911d6ca005bd9012bcc9c8a180`;
+
+//     try {
+//         const geoResponse = await fetch(geoUrl);
+//         const geoData = await geoResponse.json();
+
+//         if (geoData.length === 0) {
+//             console.log('No results found for the specified country.');
+//             return;
+//         }
+
+//         const { lat, lon } = geoData[0];
+
+//     const apiKey = '291f69f3a9ed49979d4e3cfb59f7dec9';
+//     const days = 7;
+//         const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${apiKey}lat=${lat}&lon=${lon}&days=${days}`;
+
+//         const weatherResponse = await fetch(weatherUrl);
+//         const weatherData = await weatherResponse.json();
+
+//         console.dir(countryInput)
+
+//         console.log(weatherData);
+//         createWeather(country, weatherData);
+
+//     } catch (error) {
+//         console.log('An error occurred while fetching weather data:', error);
+//     }
+// }
