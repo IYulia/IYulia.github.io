12a77246a13714040e28e35ab5de3df7f6e810ed
Yulia Tryhuba <tryhubayulia@gmail.com>
2023-06-26 18:49:03 +0300

    slider work good

--- scripts/main_page_script.js
+++ scripts/main_page_script.js
@@ -1,16 +1,31 @@
 // SLIDER
-// document.addEventListener('DOMContentLoaded', function() {
-//     var swiper = new Swiper('.offers__slider', {
-//       slidesPerView: 'auto',
-//       spaceBetween: 1,
-//       loop: true,
-//       autoplay: {
-//         delay: 1500,
-//         disableOnInteraction: false,
-//       },
-//       navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//       },
-//     });
-//   });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: true,
+    loopedSlides: Infinity,
+    loopAdditionalSlides: Infinity,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      reverseDirection: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    on: {
+      reachEnd: function () {
+        swiper.params.autoplay.reverseDirection = true;
+        swiper.autoplay.start();
+      },
+      reachBeginning: function () {
+        swiper.params.autoplay.reverseDirection = false;
+        swiper.autoplay.start();
+      },
+    },
+  });
