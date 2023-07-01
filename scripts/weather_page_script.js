6a666cf6802903e292c5337418aafe2a624fdf6b
Yulia Tryhuba <tryhubayulia@gmail.com>
2023-07-01 11:40:59 +0300

    add location page and work with weather

--- scripts/weather_page_script.js
+++ scripts/weather_page_script.js
@@ -32,6 +32,7 @@ async function searchWeather() {
 }
 
 function createWeather(country, weatherData) {
+
     const weather = weatherData.weather[0];
     const divWeatherInfo = document.getElementById('weather-info');
     let weatherDescription = weather.description;
@@ -42,8 +43,10 @@ function createWeather(country, weatherData) {
 
     let sunriseTime = getSunriseSunsetTime(sunrise);
     let sunsetTime = getSunriseSunsetTime(sunset);
+    const weatherTitel = document.getElementById('weatherTitel');
+    weatherTitel.innerHTML = country;
 
-    const html = `<h1 class="weatherTitel">${country}</h1>
+    const html = `
                   <img class="weatherImg" src="https://openweathermap.org/img/wn/${weather.icon}.png" alt="">
                   <p class="weatherDescription">${weatherDescription}</p>
                   <h2 class="weatherTemp">${temp}&#8451;</h2>
@@ -52,6 +55,31 @@ function createWeather(country, weatherData) {
                   <p class="windSpeed">Wind Speed: ${wind} m/s</p>`;
 
     divWeatherInfo.innerHTML = html;
+
+    divWeatherInfo.style.borderWidth = '1px';
+    divWeatherInfo.style.borderStyle = 'solid';
+    divWeatherInfo.style.borderColor = 'rgb(113, 113, 113)';
+    divWeatherInfo.style.borderRadius = "20px"
+
+    ///////////////////////////////
+    const divWeatherInfo2 = document.getElementById("weather-info2")
+    console.log(divWeatherInfo2)
+
+    const html2 = `
+                  <img class="weatherImg" src="https://openweathermap.org/img/wn/${weather.icon}.png" alt="">
+                  <p class="weatherDescription">${weatherDescription}</p>
+                  <h2 class="weatherTemp">${temp}&#8451;</h2>
+                  <p class="sunriseTime">Sunrise: ${sunriseTime}</p>
+                  <p class="sunsetTime">Sunset: ${sunsetTime}</p>
+                  <p class="windSpeed">Wind Speed: ${wind} m/s</p>`;
+
+    divWeatherInfo2.innerHTML = html2;
+
+    divWeatherInfo2.style.borderWidth = '1px';
+    divWeatherInfo2.style.borderStyle = 'solid';
+    divWeatherInfo2.style.borderColor = 'rgb(113, 113, 113)';
+    divWeatherInfo2.style.borderRadius = "20px"
+
 }
 
 function getSunriseSunsetTime(timestamp) {
@@ -60,43 +88,3 @@ function getSunriseSunsetTime(timestamp) {
     const timeString = date.toLocaleTimeString().substr(0, 8);
     return timeString;
 }
-
-
-// async function searchWeather() {
-//     const countryInput = document.getElementById('country');
-//     const country = countryInput.value;
-
-//     if (!country) {
-//         console.log('Please enter a country.');
-//         return;
-//     }
-
-//     const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${country}&limit=1&appid=ad0e5d911d6ca005bd9012bcc9c8a180`;
-
-//     try {
-//         const geoResponse = await fetch(geoUrl);
-//         const geoData = await geoResponse.json();
-
-//         if (geoData.length === 0) {
-//             console.log('No results found for the specified country.');
-//             return;
-//         }
-
-//         const { lat, lon } = geoData[0];
-
-//     const apiKey = '291f69f3a9ed49979d4e3cfb59f7dec9';
-//     const days = 7;
-//         const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=${apiKey}lat=${lat}&lon=${lon}&days=${days}`;
-
-//         const weatherResponse = await fetch(weatherUrl);
-//         const weatherData = await weatherResponse.json();
-
-//         console.dir(countryInput)
-
-//         console.log(weatherData);
-//         createWeather(country, weatherData);
-
-//     } catch (error) {
-//         console.log('An error occurred while fetching weather data:', error);
-//     }
-// }
