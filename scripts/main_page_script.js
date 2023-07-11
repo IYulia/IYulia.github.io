581be0341d988a8d3231bfccf4ddf551e3feb4eb
Yulia Tryhuba <tryhubayulia@gmail.com>
2023-07-11 21:48:33 +0300

    add adaptive for main page

--- scripts/main_page_script.js
+++ scripts/main_page_script.js
@@ -59,6 +59,35 @@ var swiper = new Swiper(".mySwiper", {
       theme.setAttribute('class', 'theme-light');
       document.body.removeAttribute('dark');
     }
-  });
+  });  
+
+  function burgerMenu(selector) {
+    let menu = document.querySelector(selector);
+    let button = menu.querySelector('.burger-menu_button, .burger-menu_lines');
+    let links = menu.querySelectorAll('.burger-menu_link');
+    let overlay = menu.querySelector('.burger-menu_overlay');
   
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      toggleMenu();
+    });
+  
+    links.forEach(link => {
+      link.addEventListener('click', () => toggleMenu());
+    });
+  
+    overlay.addEventListener('click', () => toggleMenu());
+  
+    function toggleMenu() {
+      menu.classList.toggle('burger-menu_active');
+  
+      if (menu.classList.contains('burger-menu_active')) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'visible';
+      }
+    }
+  }
+  
+  burgerMenu('.burger-menu');
   
